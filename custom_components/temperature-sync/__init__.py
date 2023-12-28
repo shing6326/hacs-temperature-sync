92aabbed93ca386401795b54d93915a563b128ea
Billy Chan <shing6326@gmail.com>
2023-12-29 00:57:40 +0800

    :art: update

--- custom_components/temperature-sync/__init__.py
+++ custom_components/temperature-sync/__init__.py
@@ -1,8 +1,8 @@
 """Set up the Temperature Sync component."""
 import voluptuous as vol
-
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv, discovery
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import config_validation as cv, entity_platform
+from homeassistant.const import ATTR_TEMPERATURE
 
 DOMAIN = "temperature_sync"
 CONFIG_SCHEMA = vol.Schema({
@@ -14,14 +14,28 @@ CONFIG_SCHEMA = vol.Schema({
     })
 }, extra=vol.ALLOW_EXTRA)
 
+
 def setup(hass: HomeAssistant, config: dict):
     """Set up the Temperature Sync component."""
-    hass.data[DOMAIN] = {}
+    def handle_temperature_change(event):
+        """Handle the temperature change."""
+        entity_id = event.data.get('entity_id')
+        new_state = event.data.get('new_state')
+        if new_state is None:
+            return
 
-    # Read configuration
+        # Find the corresponding climate entity
+        for pair in pairs:
+            if pair["sensor"] == entity_id:
+                climate_entity_id = pair["climate"]
+                # Update the climate entity's temperature
+                hass.states.set(climate_entity_id, new_state.state, attributes={
+                    ATTR_CURRENT_TEMPERATURE: new_state.state
+                })
+
     pairs = config[DOMAIN]["pairs"]
     for pair in pairs:
-        hass.data[DOMAIN][pair["sensor"]] = pair["climate"]
+        # Listen for changes in the temperature sensor
+        hass.bus.listen('state_changed', handle_temperature_change)
 
-    hass.helpers.discovery.load_platform('climate', DOMAIN, {}, config)
     return True
