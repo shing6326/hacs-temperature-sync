88da28dbf1e68a5e0cf41e4dd59b12f26daba147
Billy Chan <shing6326@gmail.com>
2023-12-29 14:03:43 +0800

    :zap: sync temperature when climate current_temperature sensor become null

--- custom_components/temperature-sync/__init__.py
+++ custom_components/temperature-sync/__init__.py
@@ -1,9 +1,9 @@
-"""Set up the Temperature Sync component."""
 import voluptuous as vol
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers import config_validation as cv, entity_platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv, event
 from homeassistant.components.climate.const import ATTR_CURRENT_TEMPERATURE
 import logging
+
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "temperature-sync"
@@ -16,51 +16,63 @@ CONFIG_SCHEMA = vol.Schema({
     })
 }, extra=vol.ALLOW_EXTRA)
 
-def setup(hass: HomeAssistant, config: dict):
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Temperature Sync component."""
-    def handle_temperature_change(event):
+    sensor_to_climate_map = {pair["sensor"]: pair["climate"] for pair in config[DOMAIN]["pairs"]}
+    climate_to_sensor_map = {pair["climate"]: pair["sensor"] for pair in config[DOMAIN]["pairs"]}
+
+    async def handle_temperature_change(event):
         """Handle the temperature change."""
         entity_id = event.data.get('entity_id')
         new_state = event.data.get('new_state')
 
-        if new_state is None or entity_id is None:
+        if new_state is None or new_state.state in ['unavailable', 'unknown']:
             return
 
-        # Check if the entity_id is in the sensor list we are monitoring
-        if entity_id not in [pair["sensor"] for pair in pairs]:
-            return
-
-        # Check if the sensor value is ready or not
-        if new_state.state in ['unavailable', 'unknown']:
-            return
         try:
             new_temperature = float(new_state.state)
         except ValueError:
             _LOGGER.error("State of %s is not a valid temperature: %s", entity_id, new_state.state)
             return
 
-        # Find the corresponding climate entity
-        for pair in pairs:
-            if pair["sensor"] == entity_id:
-                climate_entity_id = pair["climate"]
-                climate_state = hass.states.get(climate_entity_id)
-
-                if climate_state is None:
-                    continue
-
+        climate_entity_id = sensor_to_climate_map.get(entity_id)
+        if climate_entity_id:
+            climate_state = hass.states.get(climate_entity_id)
+            if climate_state:
                 attributes = dict(climate_state.attributes)
                 attributes[ATTR_CURRENT_TEMPERATURE] = new_temperature
+                # Using async_set instead of hass.states.set
+                hass.states.async_set(climate_entity_id, climate_state.state, attributes)
 
-                hass.states.set(climate_entity_id, climate_state.state, attributes)
+    async def handle_climate_change(event):
+        """Handle the climate change, specifically looking for lost current_temperature."""
+        entity_id = event.data.get('entity_id')
+        new_state = event.data.get('new_state')
 
+        if entity_id not in climate_to_sensor_map:
+            return
 
-    pairs = config[DOMAIN]["pairs"]
+        sensor_entity_id = climate_to_sensor_map[entity_id]
+        sensor_state = hass.states.get(sensor_entity_id)
 
-    # Listen for changes in each temperature sensor
-    for pair in pairs:
-        sensor_entity_id = pair["sensor"]
+        if sensor_state is None or sensor_state.state in ['unavailable', 'unknown']:
+            return
 
-        # Create a targeted event listener for each sensor
-        hass.bus.listen('state_changed', handle_temperature_change)
+        try:
+            new_temperature = float(sensor_state.state)
+        except ValueError:
+            _LOGGER.error("State of %s is not a valid temperature: %s", sensor_entity_id, sensor_state.state)
+            return
+
+        # Only proceed if current_temperature is missing or needs update
+        if new_state.attributes.get(ATTR_CURRENT_TEMPERATURE) is None:
+            attributes = dict(new_state.attributes)
+            attributes[ATTR_CURRENT_TEMPERATURE] = new_temperature
+            hass.states.async_set(entity_id, new_state.state, attributes)
+
+    # Track state changes for the specified sensors and climate entities
+    for sensor_entity_id, climate_entity_id in sensor_to_climate_map.items():
+        event.async_track_state_change_event(hass, [sensor_entity_id], handle_temperature_change)
+        event.async_track_state_change_event(hass, [climate_entity_id], handle_climate_change)
 
     return True
